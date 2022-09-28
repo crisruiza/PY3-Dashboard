a32e815439c9fe5e9093167645f3fe39ebb5e4af
Cristina Ruiz <cristinaruizald@outlook.com>
2022-09-27 23:45:25 -0500

    Re-nombrar variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,9 @@ console.log("Entró a main.js");
 const endPoint_Base =
   "https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations";
 const tblAoE = document.getElementById("tblAoE");
+const chartExpansion = document
+  .getElementById("chartExpansion")
+  .getContext("2d");
 
 // Obtener datos de API
 function loadData() {
@@ -13,26 +16,44 @@ function loadData() {
     .then((data) => {
       console.log(data.civilizations);
 
-      // Constante para gráfica
+      // Constante para gráfica expansion
       const expansion = data.civilizations.map((item) => item.expansion);
-      console.log(expansion);
-
+      // Función para contar instancias en el array
+      const contador_expansion = {};
+      for (const nombre of expansion) {
+        contador_expansion[nombre] = contador_expansion[nombre]
+          ? contador_expansion[nombre] + 1
+          : 1;
+      }
       // Gráficas
-      const grafica = {
-        labels: ["Red", "Blue", "Yellow"],
-        datasets: [
-          {
-            label: "My First Dataset",
-            data: [300, 50, 100],
-            backgroundColor: [
-              "rgb(255, 99, 132)",
-              "rgb(54, 162, 235)",
-              "rgb(255, 205, 86)",
-            ],
-            hoverOffset: 4,
+
+      const Expansion_chart = new Chart(chartExpansion, {
+        type: "pie",
+        data: {
+          labels: Object.keys(contador_expansion),
+          datasets: [
+            {
+              label: "# of Votes",
+              data: Object.values(contador_expansion),
+              backgroundColor: [
+                "rgba(206, 73, 80, 1)",
+                "rgba(43, 72, 78, 1)",
+                "rgba(55, 115, 116, 1)",
+                "rgba(169, 215, 205, 1)",
+                "rgba( 221, 239, 227, 01)",
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+            },
           },
-        ],
-      };
+        },
+      });
       // Información en tabla
       tblAoE.innerHTML = "";
       for (const civilizacion of data.civilizations) {
