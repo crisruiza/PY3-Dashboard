505166a6925e0d68450569de6c67ed62000f6f2f
Cristina Ruiz <cristinaruizald@outlook.com>
2022-10-06 13:58:08 -0500

    Marvel API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,72 +1,70 @@
 console.log("Entró a main.js");
 
 // Constantes
+const APIKey =
+  "?ts=1&apikey=d78c429a4ed117122d403e39b5f0cf90&hash=7bd9c3ae8ce5c02666484d0c5721a5c3";
 const endPoint_Base =
-  "https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations";
-const tblAoE = document.getElementById("tblAoE");
-const chartExpansion = document
-  .getElementById("chartExpansion")
-  .getContext("2d");
+  "https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=d78c429a4ed117122d403e39b5f0cf90&hash=7bd9c3ae8ce5c02666484d0c5721a5c3&limit=100&offset=0";
+const tblMarvel = document.getElementById("tblMarvel");
 
 // Obtener datos de API
-function loadData() {
-  console.log("Entró a loadData()...");
-  fetch(endPoint_Base)
+function getMarvel() {
+  fetch(endPoint_Base, {
+    method: "GET",
+  })
     .then((response) => response.json())
-    .then((data) => {
-      console.log(data.civilizations);
-
-      // Constante para gráfica expansion
-      const expansion = data.civilizations.map((item) => item.expansion);
-      // Función para contar instancias en el array
-      const contador_expansion = {};
-      for (const nombre of expansion) {
-        contador_expansion[nombre] = contador_expansion[nombre]
-          ? contador_expansion[nombre] + 1
-          : 1;
-      }
-      // Gráficas
-
-      const Expansion_chart = new Chart(chartExpansion, {
-        type: "pie",
-        data: {
-          labels: Object.keys(contador_expansion),
-          datasets: [
-            {
-              label: "# of Votes",
-              data: Object.values(contador_expansion),
-              backgroundColor: [
-                "rgba(206, 73, 80, 1)",
-                "rgba(43, 72, 78, 1)",
-                "rgba(55, 115, 116, 1)",
-                "rgba(169, 215, 205, 1)",
-                "rgba( 221, 239, 227, 01)",
-              ],
-              borderWidth: 1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-        },
-      });
-      // Información en tabla
-      tblAoE.innerHTML = "";
-      for (const civilizacion of data.civilizations) {
+    .then((json) => {
+      console.log(json.data.results);
+      tblMarvel.innerHTML = "";
+      for (const personaje of json.data.results) {
         let tr = `<tr>
-        <td>${civilizacion.id}</td>
-        <td>${civilizacion.name}</td>
-        <td>${civilizacion.army_type}</td>
-        <td>${civilizacion.expansion}</td>
-        <td>${civilizacion.team_bonus}</td>
-        </tr>`;
-        tblAoE.innerHTML += tr;
+  <td>${personaje.id}</td>
+  <td>${personaje.name}</td>
+ <td>${personaje.description}</td>
+  </tr>`;
+        tblMarvel.innerHTML += tr;
+        if (personaje.description === "") {
+          console.log("vacio");
+        }
       }
     });
 }
+getMarvel();
 
-loadData();
+const ctx = document.getElementById("myChart").getContext("2d");
+const myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
